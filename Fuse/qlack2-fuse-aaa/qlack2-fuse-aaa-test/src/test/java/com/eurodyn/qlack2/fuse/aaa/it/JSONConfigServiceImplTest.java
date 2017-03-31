390659efafff5b94681047364ad327c858cfa147
clebesis <constantinos.lebesis@eurodyn.com>
2017-03-31 11:18:20 +0200

    add tests

---
+++ Fuse/qlack2-fuse-aaa/qlack2-fuse-aaa-test/src/test/java/com/eurodyn/qlack2/fuse/aaa/it/JSONConfigServiceImplTest.java
@@ -0,0 +1,37 @@
+package com.eurodyn.qlack2.fuse.aaa.it;
+
+import com.eurodyn.qlack2.fuse.aaa.api.JSONConfigService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.ops4j.pax.exam.spi.reactors.PerMethod;
+import org.ops4j.pax.exam.util.Filter;
+import org.osgi.framework.Bundle;
+import javax.inject.Inject;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+
+
+/**
+ * @author European Dynamics SA
+ */
+@RunWith(PaxExam.class)
+@ExamReactorStrategy(PerMethod.class)
+public class JSONConfigServiceImplTest extends ITTestConf {
+
+    @Inject
+    @Filter(timeout = 1200000)
+    JSONConfigService jsonConfigService;
+
+
+    @Test
+    public void processBundle(){
+        //returns bundle for the specific class
+        BundleContext ctx = FrameworkUtil.getBundle(JSONConfigService.class).getBundleContext();
+        Bundle bundle = ctx.getBundle();
+        //check if has configuration file,then calls parseConfig()
+        jsonConfigService.processBundle(bundle);
+    }
+
+}
---
+++ Fuse/qlack2-fuse-aaa/qlack2-fuse-aaa-test/src/test/java/com/eurodyn/qlack2/fuse/aaa/it/VerificationServiceImplTest.java
@@ -0,0 +1,65 @@
+package com.eurodyn.qlack2.fuse.aaa.it;
+
+import com.eurodyn.qlack2.fuse.aaa.api.VerificationService;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.ops4j.pax.exam.spi.reactors.PerMethod;
+import org.ops4j.pax.exam.util.Filter;
+import javax.inject.Inject;
+
+
+/**
+ * @author European Dynamics SA
+ */
+@RunWith(PaxExam.class)
+@ExamReactorStrategy(PerMethod.class)
+public class VerificationServiceImplTest extends ITTestConf {
+
+    @Inject
+    @Filter(timeout = 1200000)
+    VerificationService verificationService;
+
+
+
+    @Test
+    public void createVerificationToken(){
+        Assert.assertNotNull(verificationService.createVerificationToken(TestConst.VERIFICATION_EXPIRES_ON,TestConst.VERIFICATION_DATA));
+    }
+
+
+    @Test
+    public void createVerificationTokenArgs(){
+        Assert.assertNotNull(verificationService.createVerificationToken(TestConst.VERIFICATION_EXPIRES_ON));
+    }
+
+
+    @Test
+    public void verifyToken(){
+        String tokenID= verificationService.createVerificationToken(TestConst.VERIFICATION_EXPIRES_ON);
+        Assert.assertNotNull(verificationService.verifyToken(tokenID));
+    }
+
+
+    @Test
+    public void deleteToken(){
+        String tokenID = verificationService.createVerificationToken(TestConst.VERIFICATION_EXPIRES_ON);
+        Assert.assertNotNull(tokenID);
+
+        verificationService.deleteToken(tokenID);
+        Assert.assertFalse(verificationService.verifyToken(tokenID));
+    }
+
+
+    @Test
+    public void getTokenPayload(){
+        String tokenID = verificationService.createVerificationToken(TestConst.VERIFICATION_EXPIRES_ON,TestConst.VERIFICATION_DATA);
+        Assert.assertNotNull(tokenID);
+
+        Assert.assertNotNull(verificationService.getTokenPayload(tokenID));
+    }
+
+
+}
