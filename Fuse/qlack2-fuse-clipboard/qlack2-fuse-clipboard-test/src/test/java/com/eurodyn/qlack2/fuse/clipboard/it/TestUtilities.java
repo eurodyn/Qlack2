c52dddab0a39bf8c5555fb8951dbba17a2ac8374
clebesis <constantinos.lebesis@eurodyn.com>
2017-05-19 12:29:01 +0200

    added fuse-clipboard tests

---
+++ Fuse/qlack2-fuse-clipboard/qlack2-fuse-clipboard-test/src/test/java/com/eurodyn/qlack2/fuse/clipboard/it/TestUtilities.java
@@ -0,0 +1,31 @@
+package com.eurodyn.qlack2.fuse.clipboard.it;
+
+import com.eurodyn.qlack2.fuse.clipboard.api.dto.ClipboardEntryDTO;
+import com.eurodyn.qlack2.fuse.clipboard.api.dto.ClipboardMetaDTO;
+import java.util.UUID;
+import java.util.Date;
+
+public class TestUtilities {
+
+    public static ClipboardEntryDTO createClipboardEntryDTO(){
+        ClipboardEntryDTO clipboardEntryDTO = new ClipboardEntryDTO();
+        clipboardEntryDTO.setId(UUID.randomUUID().toString());
+        clipboardEntryDTO.setTitle(TestConst.generateRandomString());
+        clipboardEntryDTO.setSrcUserId(UUID.randomUUID().toString());
+        clipboardEntryDTO.setCreatedOn(new Date().getTime());
+        clipboardEntryDTO.setDescription(TestConst.generateRandomString());
+        clipboardEntryDTO.setObjectId(UUID.randomUUID().toString());
+        clipboardEntryDTO.setOwnerId(UUID.randomUUID().toString());
+        return clipboardEntryDTO;
+    }
+
+    public static ClipboardMetaDTO createClipboardMetaDTO(){
+        ClipboardMetaDTO clipboardMetaDTO = new ClipboardMetaDTO();
+        clipboardMetaDTO.setId(UUID.randomUUID().toString());
+        clipboardMetaDTO.setValue(TestConst.generateRandomString());
+        clipboardMetaDTO.setSrcUserId(UUID.randomUUID().toString());
+        clipboardMetaDTO.setName(UUID.randomUUID().toString());
+        return clipboardMetaDTO;
+    }
+
+}
