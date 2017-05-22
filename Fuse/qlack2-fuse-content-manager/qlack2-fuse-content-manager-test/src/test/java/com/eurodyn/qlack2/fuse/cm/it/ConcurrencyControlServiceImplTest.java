343bf69b430a280e523f896e896a332fe1a8f402
clebesis <constantinos.lebesis@eurodyn.com>
2017-05-22 14:34:01 +0200

    added fuse-content-manager tests

---
+++ Fuse/qlack2-fuse-content-manager/qlack2-fuse-content-manager-test/src/test/java/com/eurodyn/qlack2/fuse/cm/it/ConcurrencyControlServiceImplTest.java
@@ -0,0 +1,87 @@
+package com.eurodyn.qlack2.fuse.cm.it;
+
+import com.eurodyn.qlack2.fuse.cm.api.DocumentService;
+import com.eurodyn.qlack2.fuse.cm.api.ConcurrencyControlService;
+import com.eurodyn.qlack2.fuse.cm.api.dto.FolderDTO;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.ops4j.pax.exam.spi.reactors.PerMethod;
+import org.ops4j.pax.exam.util.Filter;
+import org.junit.Assert;
+import org.junit.Test;
+import javax.inject.Inject;
+
+/**
+ * @author European Dynamics SA.
+ */
+@RunWith(PaxExam.class)
+@ExamReactorStrategy(PerMethod.class)
+public class ConcurrencyControlServiceImplTest extends ITTestConf {
+
+    @Inject
+    @Filter(timeout = 1200000)
+    DocumentService documentService;
+
+    @Inject
+    @Filter(timeout = 1200000)
+    ConcurrencyControlService concurrencyControlService;
+
+    @Test
+    public void lock(){
+        FolderDTO folderDTO = TestUtilities.createFolderDTO();
+        String folderID = documentService.createFolder(folderDTO,TestConst.userID,folderDTO.getId());
+        Assert.assertNotNull(folderID);
+
+        concurrencyControlService.lock(folderID,"locked01",false,TestConst.userID);
+        Assert.assertNotNull(concurrencyControlService.getSelectedNodeWithLockConflict(folderID,"unlocked01"));
+    }
+
+    @Test
+    public void unlock(){
+        FolderDTO folderDTO = TestUtilities.createFolderDTO();
+        String folderID = documentService.createFolder(folderDTO,TestConst.userID,folderDTO.getId());
+        Assert.assertNotNull(folderID);
+
+        concurrencyControlService.lock(folderID,folderDTO.getLockedBy(),false,TestConst.userID);
+        concurrencyControlService.unlock(folderID,folderDTO.getLockedBy(),false,TestConst.userID);
+
+        //expect null
+        Assert.assertNull(concurrencyControlService.getSelectedNodeWithLockConflict(folderID,"unlocked02"));
+    }
+
+    @Test
+    public void getSelectedNodeWithLockConflict(){
+        FolderDTO folderDTO = TestUtilities.createFolderDTO();
+        String folderID = documentService.createFolder(folderDTO,TestConst.userID,folderDTO.getId());
+        Assert.assertNotNull(folderID);
+
+        concurrencyControlService.lock(folderID,"locked03",false,TestConst.userID);
+
+        Assert.assertNotNull(concurrencyControlService.getSelectedNodeWithLockConflict(folderID,"unlocked03"));
+    }
+
+    @Test
+    public void getAncestorFolderWithLockConflict(){
+        FolderDTO folderDTO = TestUtilities.createFolderDTO();
+        String folderID = documentService.createFolder(folderDTO,TestConst.userID,folderDTO.getId());
+        Assert.assertNotNull(folderID);
+
+        concurrencyControlService.lock(folderID,"locked04",false,TestConst.userID);
+
+        Assert.assertNotNull(concurrencyControlService.getAncestorFolderWithLockConflict(folderID,"unlocked04"));
+    }
+
+    @Test
+    public void getDescendantNodeWithLockConflict(){
+        FolderDTO folderDTO = TestUtilities.createFolderDTO();
+        String folderID = documentService.createFolder(folderDTO,TestConst.userID,folderDTO.getId());
+        Assert.assertNotNull(folderID);
+
+        concurrencyControlService.lock(folderID,"locked05",false,TestConst.userID);
+
+        //Descendant node isnt locked, expcted null
+        Assert.assertNull(concurrencyControlService.getDescendantNodeWithLockConflict(folderID,"unlocked05"));
+    }
+
+}
---
+++ Fuse/qlack2-fuse-content-manager/qlack2-fuse-content-manager-test/src/test/java/com/eurodyn/qlack2/fuse/cm/it/VersionServiceImplTest.java
@@ -0,0 +1,237 @@
+package com.eurodyn.qlack2.fuse.cm.it;
+
+import com.eurodyn.qlack2.fuse.cm.api.VersionService;
+import com.eurodyn.qlack2.fuse.cm.api.DocumentService;
+import com.eurodyn.qlack2.fuse.cm.api.dto.FolderDTO;
+import com.eurodyn.qlack2.fuse.cm.api.dto.FileDTO;
+import com.eurodyn.qlack2.fuse.cm.api.dto.VersionDTO;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.ops4j.pax.exam.spi.reactors.PerMethod;
+import org.ops4j.pax.exam.util.Filter;
+import javax.inject.Inject;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author European Dynamics SA.
+ */
+@RunWith(PaxExam.class)
+@ExamReactorStrategy(PerMethod.class)
+public class VersionServiceImplTest extends ITTestConf {
+
+    @Inject
+    @Filter(timeout = 1200000)
+    VersionService versionService;
+
+    @Inject
+    @Filter(timeout = 1200000)
+    DocumentService documentService;
+
+    @Test
+    public void createVersion(){
+        FileDTO fileDTO = TestUtilities.createFileDTO();
+        String fileID = documentService.createFile(fileDTO,TestConst.userID,fileDTO.getId());
+        Assert.assertNotNull(fileID);
+
+        VersionDTO versionDTO = TestUtilities.createVersionDTO();
+        String versionID = versionService.createVersion(fileID,versionDTO,"filename01",TestConst.content,TestConst.userID,fileDTO.getId());
+        Assert.assertNotNull(versionID);
+    }
+
+    @Test
+    public void getFileVersions(){
+        FileDTO fileDTO = TestUtilities.createFileDTO();
+        String fileID = documentService.createFile(fileDTO,TestConst.userID,fileDTO.getId());
+        Assert.assertNotNull(fileID);
+
+        VersionDTO versionDTO = TestUtilities.createVersionDTO();
+        String versionID = versionService.createVersion(fileID,versionDTO,"filename02",TestConst.content,TestConst.userID,fileDTO.getId());
+        Assert.assertNotNull(versionID);
+
+        Assert.assertNotNull(versionService.getFileVersions(fileID));
+    }
+
+    @Test
+    public void getFileLatestVersion(){
+        FileDTO fileDTO = TestUtilities.createFileDTO();
+        String fileID = documentService.createFile(fileDTO,TestConst.userID,fileDTO.getId());
+        Assert.assertNotNull(fileID);
+
+        VersionDTO versionDTO = TestUtilities.createVersionDTO();
+        String versionID = versionService.createVersion(fileID,versionDTO,"filename03",TestConst.content,TestConst.userID,fileDTO.getId());
+        Assert.assertNotNull(versionID);
+
+        Assert.assertNotNull(versionService.getFileLatestVersion(fileID));
+    }
+
+    @Test
+    public void getBinContent(){
+        FileDTO fileDTO = TestUtilities.createFileDTO();
+        String fileID = documentService.createFile(fileDTO,TestConst.userID,fileDTO.getId());
+        Assert.assertNotNull(fileID);
+
+        VersionDTO versionDTO = TestUtilities.createVersionDTO();
+        String versionID = versionService.createVersion(fileID,versionDTO,"filename04",TestConst.content,TestConst.userID,fileDTO.getId());
+        Assert.assertNotNull(versionID);
+
+        Assert.assertNotNull(versionService.getBinContent(fileID));
+    }
+
+    @Test
+    public void getBinContentArgs(){
+        FileDTO fileDTO = TestUtilities.createFileDTO();
+        String fileID = documentService.createFile(fileDTO,TestConst.userID,fileDTO.getId());
+        Assert.assertNotNull(fileID);
+
+        VersionDTO versionDTO = TestUtilities.createVersionDTO();
+        String versionID = versionService.createVersion(fileID,versionDTO,"filename05",TestConst.content,TestConst.userID,fileDTO.getId());
+        Assert.assertNotNull(versionID);
+
+        Assert.assertNotNull(versionService.getBinContent(fileID,versionDTO.getName()));
+    }
+
+    @Test
+    public void getFileAsZip(){
+        FileDTO fileDTO = TestUtilities.createFileDTO();
+        String fileID = documentService.createFile(fileDTO,TestConst.userID,fileDTO.getId());
+        Assert.assertNotNull(fileID);
+
+        VersionDTO versionDTO = TestUtilities.createVersionDTO();
+        String versionID = versionService.createVersion(fileID,versionDTO,"filename06",TestConst.content,TestConst.userID,fileDTO.getId());
+        Assert.assertNotNull(versionID);
+
+        Assert.assertNotNull(versionService.getFileAsZip(fileID,false));
+    }
+
+    @Test
+    public void getFileAsZipArgs(){
+        FileDTO fileDTO = TestUtilities.createFileDTO();
+        String fileID = documentService.createFile(fileDTO,TestConst.userID,fileDTO.getId());
+        Assert.assertNotNull(fileID);
+
+        VersionDTO versionDTO = TestUtilities.createVersionDTO();
+        String versionID = versionService.createVersion(fileID,versionDTO,"filename07",TestConst.content,TestConst.userID,fileDTO.getId());
+        Assert.assertNotNull(versionID);
+
+        Assert.assertNotNull(versionService.getFileAsZip(fileID,versionDTO.getName(),false));
+    }
+
+    @Test
+    public void setBinChunk(){
+        FileDTO fileDTO = TestUtilities.createFileDTO();
+        String fileID = documentService.createFile(fileDTO,TestConst.userID,fileDTO.getId());
+        Assert.assertNotNull(fileID);
+
+        VersionDTO versionDTO = TestUtilities.createVersionDTO();
+        String versionID = versionService.createVersion(fileID,versionDTO,"filename08",TestConst.content,TestConst.userID,fileDTO.getId());
+        Assert.assertNotNull(versionID);
+
+        versionService.setBinChunk(versionID,TestConst.content,1);
+        Assert.assertNotNull(versionService.getBinContent(fileID,versionDTO.getName()));
+        Assert.assertNotNull(versionService.getBinChunk(versionID,1));
+    }
+
+    @Test
+    public void getBinChunk(){
+        FileDTO fileDTO = TestUtilities.createFileDTO();
+        String fileID = documentService.createFile(fileDTO,TestConst.userID,fileDTO.getId());
+        Assert.assertNotNull(fileID);
+
+        VersionDTO versionDTO = TestUtilities.createVersionDTO();
+        String versionID = versionService.createVersion(fileID,versionDTO,"filename09",TestConst.content,TestConst.userID,fileDTO.getId());
+        Assert.assertNotNull(versionID);
+
+        versionService.setBinChunk(versionID,TestConst.content,1);
+        Assert.assertNotNull(versionService.getBinContent(fileID,versionDTO.getName()));
+        Assert.assertNotNull(versionService.getBinChunk(versionID,1));
+    }
+
+    @Test
+    public void updateAttribute(){
+        FileDTO fileDTO = TestUtilities.createFileDTO();
+        String fileID = documentService.createFile(fileDTO,TestConst.userID,fileDTO.getId());
+        Assert.assertNotNull(fileID);
+
+        VersionDTO versionDTO = TestUtilities.createVersionDTO();
+        String versionID = versionService.createVersion(fileID,versionDTO,"filename10",TestConst.content,TestConst.userID,fileDTO.getId());
+        Assert.assertNotNull(versionID);
+
+        Map<String,String> getAtr = versionDTO.getAttributes();
+        String attr = getAtr.toString();
+
+        versionService.updateAttribute(fileID,attr,"attr01",TestConst.userID,fileDTO.getId());
+
+        Assert.assertNotNull(versionService.getFileLatestVersion(fileID).getAttributes());
+    }
+
+    @Test
+    public void updateAttributeOne(){
+        FileDTO fileDTO = TestUtilities.createFileDTO();
+        String fileID = documentService.createFile(fileDTO,TestConst.userID,fileDTO.getId());
+        Assert.assertNotNull(fileID);
+
+        VersionDTO versionDTO = TestUtilities.createVersionDTO();
+        String versionID = versionService.createVersion(fileID,versionDTO,"filename11",TestConst.content,TestConst.userID,fileDTO.getId());
+        Assert.assertNotNull(versionID);
+
+        Map<String,String> getAtr = versionDTO.getAttributes();
+        String attr = getAtr.toString();
+
+        versionService.updateAttribute(fileID,versionDTO.getName(),versionDTO.getName(),"attr02",TestConst.userID,fileDTO.getId());
+
+        Assert.assertNotNull(versionService.getFileLatestVersion(fileID).getAttributes());
+    }
+
+    @Test
+    public void updateAttributeTwo(){
+        FileDTO fileDTO = TestUtilities.createFileDTO();
+        String fileID = documentService.createFile(fileDTO,TestConst.userID,fileDTO.getId());
+        Assert.assertNotNull(fileID);
+
+        VersionDTO versionDTO = TestUtilities.createVersionDTO();
+        String versionID = versionService.createVersion(fileID,versionDTO,"filename12",TestConst.content,TestConst.userID,fileDTO.getId());
+        Assert.assertNotNull(versionID);
+
+        Map<String,String> getAtr = versionDTO.getAttributes();
+        String attr = getAtr.toString();
+
+        versionService.updateAttribute(fileID,"filename12",attr,TestConst.userID,fileDTO.getId());
+
+        Assert.assertNotNull(versionService.getFileLatestVersion(fileID).getAttributes());
+    }
+
+    @Test
+    public void deleteAttribute(){
+        FileDTO fileDTO = TestUtilities.createFileDTO();
+        String fileID = documentService.createFile(fileDTO,TestConst.userID,fileDTO.getId());
+        Assert.assertNotNull(fileID);
+
+        VersionDTO versionDTO = TestUtilities.createVersionDTO();
+        String versionID = versionService.createVersion(fileID,versionDTO,"filename13",TestConst.content,TestConst.userID,fileDTO.getId());
+        Assert.assertNotNull(versionID);
+
+
+        versionService.deleteAttribute(fileID,versionDTO.getName(),"LAST MODIFIED ON",null,null);
+        Assert.assertNull(versionService.getFileLatestVersion(fileID).getAttributes());
+    }
+
+    @Test
+    public void deleteAttributeArgs(){
+        FileDTO fileDTO = TestUtilities.createFileDTO();
+        String fileID = documentService.createFile(fileDTO,TestConst.userID,fileDTO.getId());
+        Assert.assertNotNull(fileID);
+
+        VersionDTO versionDTO = TestUtilities.createVersionDTO();
+        String versionID = versionService.createVersion(fileID,versionDTO,"filename14",TestConst.content,TestConst.userID,fileDTO.getId());
+        Assert.assertNotNull(versionID);
+
+
+        versionService.deleteAttribute(fileID,"LAST MODIFIED ON",null,null);
+        Assert.assertNull(versionService.getFileLatestVersion(fileID).getAttributes());
+    }
+
+}
