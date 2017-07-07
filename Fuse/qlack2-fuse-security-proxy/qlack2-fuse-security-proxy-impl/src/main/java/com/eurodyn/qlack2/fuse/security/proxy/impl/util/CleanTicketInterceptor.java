ba9ee4596b3387a7b94acd699bd8d7db8787b926
Nassos A. Michas <nassosmichas@me.com>
2017-07-07 13:14:30 +0300

    Formatting

--- Fuse/qlack2-fuse-security-proxy/qlack2-fuse-security-proxy-impl/src/main/java/com/eurodyn/qlack2/fuse/security/proxy/impl/util/CleanTicketInterceptor.java
+++ Fuse/qlack2-fuse-security-proxy/qlack2-fuse-security-proxy-impl/src/main/java/com/eurodyn/qlack2/fuse/security/proxy/impl/util/CleanTicketInterceptor.java
@@ -1,26 +1,25 @@
 package com.eurodyn.qlack2.fuse.security.proxy.impl.util;
 
-import java.util.logging.Logger;
-
+import com.eurodyn.qlack2.common.util.util.TokenHolder;
 import org.apache.cxf.jaxrs.interceptor.JAXRSOutInterceptor;
 import org.apache.cxf.message.Message;
 
-import com.eurodyn.qlack2.common.util.util.TokenHolder;
+import java.util.logging.Logger;
 
 /**
  * Since most web/app servers use a pool of reusable threads, you should always
- * cleanup the ticket held in a TokenHolder at the end of processing. This 
- * interceptors automated this process at the end of each CXF/JAXRS request.
- *
+ * cleanup the ticket held in a TokenHolder at the end of processing. This
+ * interceptor automates this process at the end of each CXF/JAXRS request.
  */
 public class CleanTicketInterceptor extends JAXRSOutInterceptor {
-	private static final Logger LOGGER = Logger.getLogger(CleanTicketInterceptor.class.getName());
 
-	/* (non-Javadoc)
-	 * @see org.apache.cxf.jaxrs.interceptor.JAXRSOutInterceptor#handleMessage(org.apache.cxf.message.Message)
-	 */
-	@Override
-	public void handleMessage(Message message) {
-		TokenHolder.removeToken();
-	}
+  private static final Logger LOGGER = Logger.getLogger(CleanTicketInterceptor.class.getName());
+
+  /* (non-Javadoc)
+   * @see org.apache.cxf.jaxrs.interceptor.JAXRSOutInterceptor#handleMessage(org.apache.cxf.message.Message)
+   */
+  @Override
+  public void handleMessage(Message message) {
+    TokenHolder.removeToken();
+  }
 }
--- Fuse/qlack2-fuse-security-proxy/qlack2-fuse-security-proxy-impl/src/main/java/com/eurodyn/qlack2/fuse/security/proxy/impl/util/ExtractTicketInterceptor.java
+++ Fuse/qlack2-fuse-security-proxy/qlack2-fuse-security-proxy-impl/src/main/java/com/eurodyn/qlack2/fuse/security/proxy/impl/util/ExtractTicketInterceptor.java
@@ -1,51 +1,68 @@
 package com.eurodyn.qlack2.fuse.security.proxy.impl.util;
 
+import com.eurodyn.qlack2.common.util.util.TokenHolder;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.cxf.jaxrs.interceptor.JAXRSInInterceptor;
+import org.apache.cxf.message.Message;
+
 import java.text.MessageFormat;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.lang3.StringUtils;
-import org.apache.cxf.jaxrs.interceptor.JAXRSInInterceptor;
-import org.apache.cxf.message.Message;
-
-import com.eurodyn.qlack2.common.util.util.TokenHolder;
-
 /**
  * Extracts the QLACK Fuse IDM Ticket from the header of a request and places it
  * into a ThreadLocal variable.
- *
  */
 public class ExtractTicketInterceptor extends JAXRSInInterceptor {
-	 private static final Logger LOGGER = Logger.getLogger(ExtractTicketInterceptor.class.getName());
-	 
-	// The name of the header to look for the security ticket.
-	private String ticketHeaderName;
-	
-	public void setTicketHeaderName(String ticketHeaderName) {
-		this.ticketHeaderName = ticketHeaderName;
-	}
 
-	@Override
-	public void handleMessage(Message message) {
-		// A quick sanity check for developers.
-		if (StringUtils.isBlank(ticketHeaderName) || ticketHeaderName.indexOf("{") > -1) {
-			LOGGER.log(Level.WARNING, MessageFormat.format(
-					"It seems you have not set the name of the"
-					+ " ticket to extract. Currently set name: {0}.", ticketHeaderName));
-		}
-		
-		// Check if the request contains a security token and inject it.
-		@SuppressWarnings("unchecked")
-		List<String> list = 
-			((Map<String, List<String>>)message.get(Message.PROTOCOL_HEADERS)).get(ticketHeaderName);
-		if (CollectionUtils.isNotEmpty(list)) {
-			TokenHolder.setToken(list.get(0));
-		} else {
-			LOGGER.log(Level.FINE, 
-					"Could not find a ticket on this request.");
-		}
-	}
+  private static final Logger LOGGER = Logger.getLogger(ExtractTicketInterceptor.class.getName());
+  private static final ObjectMapper MAPPER = new ObjectMapper();
+
+  /** The name of the header to look for the security ticket. */
+  private String ticketHeaderName;
+
+  /** A helper setting to dump all incoming headers to log. Never leave this on in production. */
+  private boolean dumpHeaders;
+
+  public void setTicketHeaderName(String ticketHeaderName) {
+    this.ticketHeaderName = ticketHeaderName;
+  }
+
+  public void setDumpHeaders(boolean dumpHeaders) {
+    this.dumpHeaders = dumpHeaders;
+  }
+
+  @Override
+  public void handleMessage(Message message) {
+    /** A quick sanity check for developers. */
+    if (StringUtils.isBlank(ticketHeaderName) || ticketHeaderName.indexOf("{") > -1) {
+      LOGGER.log(Level.WARNING, MessageFormat.format(
+        "It seems you have not set the name of the"
+          + " ticket to extract. Currently set name: {0}.", ticketHeaderName));
+    }
+
+    /** Check if the request contains a security token and inject it. */
+    @SuppressWarnings("unchecked")
+    final Map<String, List<String>> headers = (Map<String, List<String>>) message.get(Message.PROTOCOL_HEADERS);
+
+    try {
+      System.out.println(MAPPER.writerWithDefaultPrettyPrinter().writeValueAsString(headers));
+    } catch (JsonProcessingException e) {
+      e.printStackTrace();
+    }
+
+    List<String> list =
+      ((Map<String, List<String>>) message.get(Message.PROTOCOL_HEADERS)).get(ticketHeaderName);
+    if (CollectionUtils.isNotEmpty(list)) {
+      TokenHolder.setToken(list.get(0));
+    } else {
+      LOGGER.log(Level.FINE,
+        "Could not find a ticket on this request.");
+    }
+  }
 }
