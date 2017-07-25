8d0a1e31039748af4f9aac0e8c1c51a35f9749d6
Nassos A. Michas <nassosmichas@me.com>
2017-07-25 16:19:14 +0300

    Added support for custom entityID in metadata XML.

--- Util/qlack2-util-sso/src/main/java/com/eurodyn/qlack2/util/sso/CustomMetadataService.java
+++ Util/qlack2-util-sso/src/main/java/com/eurodyn/qlack2/util/sso/CustomMetadataService.java
@@ -3,6 +3,7 @@ package com.eurodyn.qlack2.util.sso;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.cxf.rs.security.saml.sso.MetadataService;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -17,19 +18,32 @@ public class CustomMetadataService extends MetadataService {
   /**
    * Various XML paths to be used while manipulating the metadata
    */
-  private static final String XML_TAG_MD_NS = "urn:oasis:names:tc:SAML:2.0:metadata";
+  private static final String XML_NS_MD = "urn:oasis:names:tc:SAML:2.0:metadata";
+
   private static final String XML_TAG_SIGNATURE = "Signature";
   private static final String XML_TAG_KEYDESCRIPTOR = "KeyDescriptor";
   private static final String XML_TAG_SPSSODESCRIPTOR = "SPSSODescriptor";
   private static final String XML_TAG_SINGLELOGOUTSERVICE = "SingleLogoutService";
+  private static final String XML_TAG_ENTITYDESCRIPTOR = "EntityDescriptor";
+
+  private static final String XML_ATTRIBUTE_ENTITYID = "entityID";
 
   /**
    * Skips the <Signature> part of the metadata
    */
   private boolean skipSignature;
 
+  /**
+   * Sets a custom `entityID` in `md:EntityDescriptor` element.
+   */
+  private String entityId;
+
   public void setSkipSignature(boolean skipSignature) {
     this.skipSignature = skipSignature;
+  }
+
+  public void setEntityId(String entityId) {
+    this.entityId = entityId;
   }
 
   @Override
@@ -46,12 +60,19 @@ public class CustomMetadataService extends MetadataService {
 
     /** Rearrange XML to be compliant with the underlying schema */
     Element keyDescriptorElement = (Element) metadata
-      .getElementsByTagNameNS(XML_TAG_MD_NS, XML_TAG_KEYDESCRIPTOR).item(0);
+      .getElementsByTagNameNS(XML_NS_MD, XML_TAG_KEYDESCRIPTOR).item(0);
     keyDescriptorElement.getParentNode().removeChild(keyDescriptorElement);
     Node singleLogoutNode = metadata
-      .getElementsByTagNameNS(XML_TAG_MD_NS, XML_TAG_SINGLELOGOUTSERVICE).item(0);
-    metadata.getElementsByTagNameNS(XML_TAG_MD_NS, XML_TAG_SPSSODESCRIPTOR).item(0)
+      .getElementsByTagNameNS(XML_NS_MD, XML_TAG_SINGLELOGOUTSERVICE).item(0);
+    metadata.getElementsByTagNameNS(XML_NS_MD, XML_TAG_SPSSODESCRIPTOR).item(0)
       .insertBefore(keyDescriptorElement, singleLogoutNode);
+
+    /** Change the entityID, if requested so */
+    if (StringUtils.isNotBlank(entityId)) {
+      Element entityDescriptorElement = (Element) metadata
+        .getElementsByTagNameNS(XML_NS_MD, XML_TAG_ENTITYDESCRIPTOR).item(0);
+      entityDescriptorElement.setAttribute(XML_ATTRIBUTE_ENTITYID, entityId);
+    }
 
     return metadata;
   }
