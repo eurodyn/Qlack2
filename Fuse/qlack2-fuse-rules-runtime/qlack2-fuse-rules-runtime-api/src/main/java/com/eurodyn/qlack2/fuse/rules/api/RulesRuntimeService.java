229ef62d2f160d855a4af04714206030d81bc471
Apostolos Samaras <Apostolos.Samaras@eurodyn.com>
2017-12-21 12:08:56 +0200

    Improve javadoc

--- Fuse/qlack2-fuse-rules-runtime/qlack2-fuse-rules-runtime-api/src/main/java/com/eurodyn/qlack2/fuse/rules/api/RulesRuntimeService.java
+++ Fuse/qlack2-fuse-rules-runtime/qlack2-fuse-rules-runtime-api/src/main/java/com/eurodyn/qlack2/fuse/rules/api/RulesRuntimeService.java
@@ -14,12 +14,18 @@ public interface RulesRuntimeService {
 	StatelessExecutionResults statelessExecute(String kbaseId, String rule, Map<String, byte[]> globals, List<byte[]> facts);
 
   /**
-   * Stateless rules execution.
+   * Direct stateless rules execution.
+   * <br>
+   * A stateless session can be called like a function passing it some data and then receiving some results back.
+	 * <p>
+	 * A classloader which have the Classes of the facts is needed to be passed as argument, because the
+	 * default Drools implementation is to use the Thread classloader which may not have the needed Classes.
+	 * Usually the callers classloader is used ( e.g. this.getClass().getClassLoader() )
    *
-   * @param rules the business rules
-   * @param facts the objects against which to execute the rules
-   * @param globals named objects, visible to the rule engine
-	 * @param classLoader the classloader to be used by the rules to load fact classes
+   * @param rules the contents of the Drools (.drl) files
+   * @param facts the objects to execute against the rules
+   * @param globals global variables, available to the rules
+	 * @param classLoader to load the Classes of the facts
    */
   void statelessExecute(List<String> rules, List<Object> facts, Map<String, Object> globals, ClassLoader classLoader);
 
