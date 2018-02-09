3062dc3c2b1837098b827a1e8acfc38e11168ee1
Nassos Michas <nassosmichas@me.com>
2018-02-09 09:48:11 +0200

    Ported to new Fuse Imaging.

--- WebDesktopApps/qlack2-wdapps-my-photo/qlack2-wdapps-my-photo-impl/src/main/java/com/eurodyn/qlack2/webdesktop/apps/myphoto/resource/MyPhotoResource.java
+++ WebDesktopApps/qlack2-wdapps-my-photo/qlack2-wdapps-my-photo-impl/src/main/java/com/eurodyn/qlack2/webdesktop/apps/myphoto/resource/MyPhotoResource.java
@@ -1,5 +1,11 @@
 package com.eurodyn.qlack2.webdesktop.apps.myphoto.resource;
 
+import com.eurodyn.qlack2.fuse.fileupload.api.FileUpload;
+import com.eurodyn.qlack2.fuse.fileupload.api.response.FileGetResponse;
+import com.eurodyn.qlack2.fuse.imaging.api.ImagingService;
+import com.eurodyn.qlack2.fuse.imaging.api.util.ResamplingAlgorithm;
+import com.eurodyn.qlack2.webdesktop.apps.myphoto.api.Constants;
+import com.eurodyn.qlack2.webdesktop.apps.myphoto.api.MyPhotoService;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import javax.ws.rs.GET;
@@ -8,50 +14,48 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
-
 import org.ops4j.pax.cdi.api.OsgiService;
-
-import com.eurodyn.qlack2.fuse.fileupload.api.FileUpload;
-import com.eurodyn.qlack2.fuse.fileupload.api.response.FileGetResponse;
-import com.eurodyn.qlack2.fuse.imaging.api.ImageService;
-import com.eurodyn.qlack2.fuse.imaging.api.util.ImageFormats;
-import com.eurodyn.qlack2.webdesktop.apps.myphoto.api.Constants;
-import com.eurodyn.qlack2.webdesktop.apps.myphoto.api.MyPhotoService;
 
 @Path("/myphoto")
 @Singleton
 public class MyPhotoResource {
-	@Inject @OsgiService
-	FileUpload fileUpload;
-	
-	@Inject
-	MyPhotoService myPhotoService;
-	
-	@Inject
-	@OsgiService
-	private ImageService imageService;
-	
-	@PUT
-	public Response updatePhoto(@QueryParam("fileID") String fileID) {
-		// Get the photo that was uploaded.
-		FileGetResponse file = fileUpload.getByID(fileID);
-		
-		// Resize the logo and make it PNG.
-		byte[] pngLogo = imageService.convertImage(file.getFile().getFileData(), ImageFormats.PNG);
-		byte[] scaledImage = imageService.scaleImage(pngLogo, Constants.PHOTO_WIDTH, Constants.PHOTO_HEIGHT); 
-		
-		myPhotoService.updateMyPhoto(scaledImage);
-	
-		fileUpload.deleteByID(fileID);
-		
-		return Response.ok().build();
-	}
-	
-	@GET
-	@Produces("image/png")
-	public Response getPhoto() {
-		byte[] photo = myPhotoService.getMyPhoto();
-		
-		return Response.ok(photo).build();
-	}
+
+  @Inject
+  @OsgiService
+  FileUpload fileUpload;
+
+  @Inject
+  MyPhotoService myPhotoService;
+
+  @Inject
+  @OsgiService
+  private ImagingService imagingService;
+
+  // Target format for image conversion.
+  private static final String IMAGE_FORMAT = "PNG";
+
+  @PUT
+  public Response updatePhoto(@QueryParam("fileID") String fileID) {
+    // Get the photo that was uploaded.
+    FileGetResponse file = fileUpload.getByID(fileID);
+
+    // Resize the logo and make it PNG.
+    byte[] png = imagingService.convert(file.getFile().getFileData(), IMAGE_FORMAT);
+    byte[] scaledPNG = imagingService.resample(png, Constants.PHOTO_WIDTH, Constants.PHOTO_HEIGHT,
+      ResamplingAlgorithm.FILTER_CUBIC);
+
+    myPhotoService.updateMyPhoto(scaledPNG);
+
+    fileUpload.deleteByID(fileID);
+
+    return Response.ok().build();
+  }
+
+  @GET
+  @Produces("image/png")
+  public Response getPhoto() {
+    byte[] photo = myPhotoService.getMyPhoto();
+
+    return Response.ok(photo).build();
+  }
 }
