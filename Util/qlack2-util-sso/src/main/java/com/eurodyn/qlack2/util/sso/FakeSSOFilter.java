d7ff1bca2fa0c1203e93f4b79c58ff6488f87332
kperp <konstantinos.perperidis@eurodyn.com>
2018-02-11 23:06:03 +0200

    Extension on FakesSSOFilter to get attributes from request - also set uniquely identifiable cookies via UUID and store them together with submitted SAMLAttributes

--- Util/qlack2-util-sso/src/main/java/com/eurodyn/qlack2/util/sso/FakeSSOFilter.java
+++ Util/qlack2-util-sso/src/main/java/com/eurodyn/qlack2/util/sso/FakeSSOFilter.java
@@ -3,14 +3,19 @@ package com.eurodyn.qlack2.util.sso;
 
 import com.eurodyn.qlack2.util.sso.dto.SAMLAttributeDTO;
 import com.eurodyn.qlack2.util.sso.dto.WebSSOHolder;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import javax.ws.rs.core.Cookie;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * This is a helper filter to allow testing of your application without an IdP. It aims to replace
@@ -27,6 +32,17 @@ public class FakeSSOFilter implements ContainerRequestFilter, ContainerResponseF
    * <pre>attr1,my-attr1-value,attr2,my-attr2-value</pre>
    */
   private List<String> fakeAttributes;
+
+  private static Map<String, List<SAMLAttributeDTO>> sessions = new ConcurrentHashMap<>();
+
+  public List<SAMLAttributeDTO> getFakeAttributesAsSAMLAttributesFromRequestParms(String uid,
+      String email, List<String> roles) {
+    List<SAMLAttributeDTO> samlAttrs = new ArrayList<>();
+    samlAttrs.add(new SAMLAttributeDTO("uid", uid));
+    samlAttrs.add(new SAMLAttributeDTO("email", email));
+    samlAttrs.add(new SAMLAttributeDTO("role", StringUtils.join(roles, ",")));
+    return samlAttrs;
+  }
 
   public List<SAMLAttributeDTO> getFakeAttributesAsSAMLAttributes() {
     List<SAMLAttributeDTO> retVal = new ArrayList<>();
@@ -48,14 +64,58 @@ public class FakeSSOFilter implements ContainerRequestFilter, ContainerResponseF
 
   @Override
   public void filter(ContainerRequestContext requestContext) throws IOException {
-    WebSSOHolder.setAttributes(getFakeAttributesAsSAMLAttributes());
+    List<SAMLAttributeDTO> samlAttrs = new ArrayList<>();
+    String cookieFromRequest = getCookieValue(requestContext);
+    if (cookieFromRequest != null) {
+      Entry<String, Cookie> cookieEntry = extractCookieFromRequestContext(requestContext);
+      if (cookieEntry != null && sessions.get(getCookieValue(requestContext)) != null) {
+        samlAttrs = sessions.get(getCookieValue(requestContext));
+      }
+    }
+
+    if (getCookieValue(requestContext) != null && requestContext.getUriInfo().getQueryParameters() != null && !requestContext.getUriInfo().getQueryParameters().isEmpty()) {
+      List<String> uid = requestContext.getUriInfo().getQueryParameters().get("uid");
+      List<String> email = requestContext.getUriInfo().getQueryParameters().get("email");
+      List<String> role = requestContext.getUriInfo().getQueryParameters().get("role");
+      if (uid != null && email != null && role != null && !uid.isEmpty() && !email.isEmpty()
+          && !role.isEmpty()) {
+        samlAttrs = getFakeAttributesAsSAMLAttributesFromRequestParms(uid.get(0), email.get(0), role);
+        sessions.put(getCookieValue(requestContext), samlAttrs);
+      }
+    }
+    if (samlAttrs.isEmpty()) {
+      samlAttrs = getFakeAttributesAsSAMLAttributes();
+    }
+    WebSSOHolder.setAttributes(samlAttrs);
   }
 
   @Override
   public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
     throws IOException {
+    String cookieUuid = UUID.randomUUID().toString();
+    String cookieFromRequest = getCookieValue(requestContext);
+    if (cookieFromRequest != null) {
+      cookieUuid = cookieFromRequest;
+    }
     /** Set a fake Cookie, so that front-end checks believe the user was properly authenticated. */
     responseContext.getHeaders().add("Set-Cookie", Constants.WEB_SSO_COOKIE_NAME +
-      "=fake-sso-cookie; Path=/");
+      "=fake-sso-cookie_" + cookieUuid + "; Path=/");
+  }
+
+  private String getCookieValue(ContainerRequestContext requestContext) {
+    Entry<String, Cookie> cookieEntry = extractCookieFromRequestContext(requestContext);
+    if (cookieEntry != null) {
+      return getUuidFromCookie(cookieEntry.getValue().getValue());
+    }
+    return null;
+  }
+
+  private Entry<String, Cookie> extractCookieFromRequestContext(ContainerRequestContext requestContext) {
+    return requestContext.getCookies().entrySet().stream()
+        .filter(k -> Constants.WEB_SSO_COOKIE_NAME.equals(k.getKey())).findFirst().orElse(null);
+  }
+
+  private String getUuidFromCookie(String cxfCookie) {
+    return cxfCookie.substring(cxfCookie.lastIndexOf("_") + 1);
   }
 }
